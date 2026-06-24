20bd4eb518b90d0c81fbce7cb703feb52f86f5a6
David Stone <david@nnucomputerwhiz.com>
2026-06-24 13:00:45 -0600

    fix: skip cache flush without cache plugins

--- Model/Cache/FlushSkipper.php
+++ Model/Cache/FlushSkipper.php
@@ -8,6 +8,9 @@
 
 namespace Mageprince\MageAI\Model\Cache;
 
+use Magento\Framework\App\Cache\StateInterface;
+use Magento\Framework\App\Cache\TypeListInterface;
+
 class FlushSkipper
 {
     /**
@@ -16,7 +19,27 @@ class FlushSkipper
     private $depth = 0;
 
     /**
-     * Run a callback while cache clean/flush/invalidation plugins are disabled.
+     * @var StateInterface
+     */
+    private $cacheState;
+
+    /**
+     * @var TypeListInterface
+     */
+    private $typeList;
+
+    /**
+     * @param StateInterface $cacheState
+     * @param TypeListInterface $typeList
+     */
+    public function __construct(StateInterface $cacheState, TypeListInterface $typeList)
+    {
+        $this->cacheState = $cacheState;
+        $this->typeList = $typeList;
+    }
+
+    /**
+     * Run a callback while cache types are disabled in the current process.
      *
      * @param callable $callback
      * @return mixed
@@ -24,10 +47,19 @@ class FlushSkipper
     public function run(callable $callback)
     {
         $this->depth++;
+        $previousStatuses = [];
+
+        if ($this->depth === 1) {
+            $previousStatuses = $this->disableCacheTypes();
+        }
+
         try {
             return $callback();
         } finally {
-            $this->depth = max(0, $this->depth - 1);
+            if ($this->depth === 1) {
+                $this->restoreCacheTypes($previousStatuses);
+            }
+            $this->depth--;
         }
     }
 
@@ -39,5 +71,39 @@ class FlushSkipper
     public function isActive(): bool
     {
         return $this->depth > 0;
+    }
+
+    /**
+     * Disable all declared cache types without persisting cache-status changes.
+     *
+     * @return array<string, bool>
+     */
+    private function disableCacheTypes(): array
+    {
+        $statuses = [];
+        foreach ($this->typeList->getTypes() as $type) {
+            $typeCode = (string) ($type['id'] ?? '');
+            if ($typeCode === '') {
+                continue;
+            }
+
+            $statuses[$typeCode] = $this->cacheState->isEnabled($typeCode);
+            $this->cacheState->setEnabled($typeCode, false);
+        }
+
+        return $statuses;
+    }
+
+    /**
+     * Restore cache types to their previous in-process statuses.
+     *
+     * @param array<string, bool> $statuses
+     * @return void
+     */
+    private function restoreCacheTypes(array $statuses): void
+    {
+        foreach ($statuses as $typeCode => $isEnabled) {
+            $this->cacheState->setEnabled($typeCode, $isEnabled);
+        }
     }
 }
